263ba1c7766f34bc317764a39f384374374a71fd
GloBoiVic <victorike08@gmail.com>
2023-07-12 15:00:14 -0500

    Initial commit

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 // Global State management variables
-let previousValue = "";
-let currentValue = "";
-let operator = "";
+let previousValue = '';
+let currentValue = '';
+let operator = '';
 let operatorActive = false;
 let result = 0;
 
 // DOM Elements;
-const numBtns = document.querySelectorAll(".num-btn");
-const operatorBtns = document.querySelectorAll(".operator-btn");
-const decimalBtn = document.querySelector(".decimal-btn");
-const clearBtn = document.querySelector(".clear-btn");
-const equalBtn = document.querySelector(".equal-btn");
-const previousOutput = document.querySelector(".previous");
-const currentOutput = document.querySelector(".current");
+const numBtns = document.querySelectorAll('.num-btn');
+const operatorBtns = document.querySelectorAll('.operator-btn');
+const decimalBtn = document.querySelector('.decimal-btn');
+const clearBtn = document.querySelector('.clear-btn');
+const equalBtn = document.querySelector('.equal-btn');
+const previousOutput = document.querySelector('.previous');
+const currentOutput = document.querySelector('.current');
 
 // Functions
 const add = function (a, b) {
@@ -25,7 +25,7 @@ const subtract = function (a, b) {
 
 const divide = function (a, b) {
   if (a === 0 || b === 0) {
-    return "Crash💥";
+    return 'Crash💥';
   } else {
     return a / b;
   }
@@ -36,32 +36,32 @@ const multiply = function (a, b) {
 };
 
 function operate(operator, num1, num2) {
-  if (operator === "add") return add(num1, num2);
-  if (operator === "subtract") return subtract(num1, num2);
-  if (operator === "divide") return divide(num1, num2);
-  if (operator === "multiply") return multiply(num1, num2);
+  if (operator === 'add') return add(num1, num2);
+  if (operator === 'subtract') return subtract(num1, num2);
+  if (operator === 'divide') return divide(num1, num2);
+  if (operator === 'multiply') return multiply(num1, num2);
 }
 
 function updateUI() {
   numBtns.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
+    btn.addEventListener('click', (e) => {
       handleNum(e.target.textContent);
       currentOutput.textContent = currentValue;
     });
   });
 
   operatorBtns.forEach((op) => {
-    op.addEventListener("click", (e) => {
-      handleOperator(e.target.getAttribute("data-action"));
+    op.addEventListener('click', (e) => {
+      handleOperator(e.target.getAttribute('data-action'));
       currentOutput.textContent = currentValue;
     });
   });
 
-  decimalBtn.addEventListener("click", addDecimal);
+  decimalBtn.addEventListener('click', addDecimal);
 
-  equalBtn.addEventListener("click", displayResult);
+  equalBtn.addEventListener('click', displayResult);
 
-  clearBtn.addEventListener("click", clear);
+  clearBtn.addEventListener('click', clear);
 }
 
 function handleNum(num) {
@@ -72,15 +72,15 @@ function handleNum(num) {
 }
 // Map data-action values to operator symbols
 const operatorSymbols = {
-  add: "+",
-  subtract: "-",
-  multiply: "x",
-  divide: "÷",
+  add: '+',
+  subtract: '-',
+  multiply: 'x',
+  divide: '÷',
 };
 
 function handleOperator(op) {
-  if (currentValue !== "") {
-    if (previousValue !== "") {
+  if (currentValue !== '') {
+    if (previousValue !== '') {
       // If both previousValue and currentValue exist, perform the calculation
       result = roundResults(operate(operator, +previousValue, +currentValue));
       previousOutput.textContent = result;
@@ -91,16 +91,16 @@ function handleOperator(op) {
     operator = op;
     operatorActive = true;
     previousValue = result;
-    currentValue = "";
+    currentValue = '';
   }
   previousOutput.textContent = `${previousValue} ${operatorSymbols[op]}`;
 }
 
 // Make decimal btn work
 function addDecimal() {
-  if (!currentValue.includes(".")) {
-    currentValue += ".";
-    currentOutput.textContent += ".";
+  if (!currentValue.includes('.')) {
+    currentValue += '.';
+    currentOutput.textContent += '.';
   }
 }
 
@@ -109,21 +109,21 @@ function roundResults(num) {
 }
 
 function displayResult() {
-  if (currentValue !== "" && previousValue !== "") {
-    result = roundResults(operate(operator, +previousValue, +currentValue));
+  if (currentValue !== '' && previousValue !== '') {
+    result = operate(operator, +previousValue, +currentValue);
     currentOutput.textContent = result;
-    previousOutput.textContent = "";
-    previousValue = "";
+    previousOutput.textContent = '';
+    previousValue = '';
     currentValue = result;
   }
 }
 
 function clear() {
-  previousValue = "";
-  currentValue = "";
+  previousValue = '';
+  currentValue = '';
   result = 0;
-  previousOutput.textContent = "";
-  currentOutput.textContent = "0";
+  previousOutput.textContent = '';
+  currentOutput.textContent = '0';
 }
 
 updateUI();
